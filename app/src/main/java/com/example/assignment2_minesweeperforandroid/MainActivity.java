a006060a1c985104a1052e26cd1e45d8fd0184ef
Charlie <34101375+smulgrew@users.noreply.github.com>
2020-12-08 17:09:04 +0000

    MineSweeper - 1st Commit As Project is Created...

--- app/src/main/java/com/example/assignment2_minesweeperforandroid/MainActivity.java
+++ app/src/main/java/com/example/assignment2_minesweeperforandroid/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.print("My First Commit As Project Now Opened");
+        System.out.print("My First Commit As Project Now Opened - Stephen");
     }
 }
